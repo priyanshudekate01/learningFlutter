42a38cd07490c989ffa7c4b2254df4411f3dc702
Priyanhsu Dekate <priyanshudekate@gmail.com>
2024-07-07 11:20:53 +0530

    decoration in container

--- lib/main.dart
+++ lib/main.dart
@@ -41,82 +41,48 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
 
-    var names = ['Raman', 'Aman', 'Mighty', 'Xeno'];
-
     return Scaffold(
-      appBar: AppBar(
+        appBar: AppBar(
 
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
 
-        title: const Center(child: Text('Flutter Container')),
-      ),
-        // ListView(
-        //     scrollDirection: Axis.horizontal,
-        //     children:[
-        //       Padding(
-        //         padding: const EdgeInsets.all(8.0),
-        //         child: Text('One', style: TextStyle(fontSize:23, fontWeight: FontWeight.w500)),
-        //       ),
-        //       Padding(
-        //         padding: const EdgeInsets.all(8.0),
-        //         child: Text('Two', style: TextStyle(fontSize:23, fontWeight: FontWeight.w500)),
-        //       ),
-        //       Padding(
-        //         padding: const EdgeInsets.all(8.0),
-        //         child: Text('Three', style: TextStyle(fontSize:23, fontWeight: FontWeight.w500)),
-        //       ),
-        //       Padding(
-        //         padding: const EdgeInsets.all(8.0),
-        //         child: Text('Four', style: TextStyle(fontSize:23, fontWeight: FontWeight.w500)),
-        //       ),
-        //       Padding(
-        //         padding: const EdgeInsets.all(8.0),
-        //         child: Text('Five', style: TextStyle(fontSize:23, fontWeight: FontWeight.w500)),
-        //       ),
-        //       Padding(
-        //         padding: const EdgeInsets.all(8.0),
-        //         child: Text('Six', style: TextStyle(fontSize:23, fontWeight: FontWeight.w500)),
-        //       ),
-        //       Padding(
-        //         padding: const EdgeInsets.all(8.0),
-        //         child: Text('Seven', style: TextStyle(fontSize:23, fontWeight: FontWeight.w500)),
-        //       ),
-        //       Padding(
-        //         padding: const EdgeInsets.all(8.0),
-        //         child: Text('Eight', style: TextStyle(fontSize:23, fontWeight: FontWeight.w500)),
-        //       ),
-        //
-        //     ]
-        // )
-
-      body: ListView.separated(itemBuilder: (context, index) {
-        return Row(
-          children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(names[index],style: TextStyle(fontSize : 21, fontWeight: FontWeight.w500),),
-          ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text(names[index],style: TextStyle(fontSize : 21, fontWeight: FontWeight.w500),),
+          title: Text('Dashboard'),
         ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text(names[index],style: TextStyle(fontSize : 21, fontWeight: FontWeight.w500),),
+        body:Container(
+          width: double.infinity,
+          height: double.infinity,
+          color: Colors.blue.shade50,
+          child: Center(
+            child: Container(
+              width: 150,
+              height: 150,
+
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft:  Radius.circular(20),
+                  bottomRight: Radius.circular(20),
+                ),
+                border: Border.all(
+                  color: Colors.black,
+                  width: 2,
+                ),
+                boxShadow:[
+                  BoxShadow(
+                    blurRadius: 10,
+                    spreadRadius: 7,
+                    color: Colors.grey
+                  )
+                ] ,
+                shape: BoxShape.rectangle,
+                // borderRadius: BorderRadius.circular(20),
+                color: Colors.blueGrey,
+              ),
+            ),
+          ),
         )
-          ],
-        );
-      },
-      itemCount: names.length,
-        separatorBuilder: (context, index){
-        return Divider(height: 100,thickness: 2,);
-        },
-      )
 
 
-
-
-       // This trailing comma makes auto-formatting nicer for build methods.
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
